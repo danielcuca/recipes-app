bc15c7e7ec2e5b9764c420259f349f112746c516
Milena Cordeiro <mcs9823@gmail.com>
2023-06-05 17:31:17 -0300

    Requisito 7 - implementando header de acordo com a necessidade de cada tela

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,76 @@
-import React from 'react';
+import React, { useLocation } from 'react';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
+  const { pathname } = useLocation();
+
+  let pageTitle = '';
+
+  switch (pathname) {
+  case '/meals':
+    pageTitle = 'Meals';
+    break;
+
+  case '/drinks':
+    pageTitle = 'Drinks';
+    break;
+
+  case '/profile':
+    pageTitle = 'Profile';
+    break;
+  case '/done-recipes':
+    pageTitle = 'Done Recipes';
+    break;
+  case '/favorite-recipes':
+    pageTitle = 'Favorite Recipes';
+    break;
+  default:
+    pageTitle = 'App de receitas';
+    break;
+  }
+
+  const isMeals = pathname === '/meals';
+  const isDrinks = pathname === '/drinks';
+  const isProfile = pathname === '/profile';
+  const isDone = pathname === '/done-recipes';
+  const isFavorite = pathname === '/favorites';
+
   return (
-    <div>Header</div>
+    <header>
+      <h1 data-testeid="page-title">{pageTitle}</h1>
+
+      {(isMeals || isDrinks) && (
+        <button type="button" data-testid="profile-top-btn">
+          <img src={ profileIcon } alt="profile-btn" />
+        </button>
+      )}
+
+      {(isMeals || isDrinks) && (
+        <button data-testid="search-top-btn">
+          <img src={ searchIcon } alt="search-btn" />
+        </button>
+      )}
+
+      {isProfile && (
+        <button type="button" data-testid="profile-top-btn">
+          <img src={ profileIcon } alt="profile-btn" />
+        </button>
+      )}
+
+      {isDone && (
+        <button type="button" data-testid="profile-top-btn">
+          <img src={ profileIcon } alt="profile-btn" />
+        </button>
+      )}
+
+      {isFavorite && (
+        <button type="button" data-testid="profile-top-btn">
+          <img src={ profileIcon } alt="profile-btn" />
+        </button>
+      )}
+
+    </header>
   );
 }
 
