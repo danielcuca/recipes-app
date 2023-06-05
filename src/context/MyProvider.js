d1414872174d91f3242df409139cd826756eb017
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-06-05 16:05:06 -0300

    Criado componente de loading e iniciado a pasta context configurada.

---
+++ src/context/MyProvider.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import MyContext from './MyContext';
+
+function Provider({ children }) {
+  const [isLoading, setLoading] = useState(true);
+
+  const value = useMemo(() => ({
+    isLoading,
+    setLoading,
+  }), []);
+
+  return (
+    <MyContext.Provider value={ value }>
+      {children}
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default Provider;
